bca39c65200a01e5626e278489e2b3b62ca5a4b3
Chris Gilchrist <gilly@sgnet.co.uk>
2021-08-04 22:30:51 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { CatDto } from './entities/catDto';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'client';
+
+  cats: Array<CatDto> = [];
+
+  constructor(private http: HttpClient) {
+
+    this.http.get('http://localhost:3000/cats').subscribe((cats: Array<CatDto>) => {
+      this.cats = cats;
+    });
+
+  }
 }
